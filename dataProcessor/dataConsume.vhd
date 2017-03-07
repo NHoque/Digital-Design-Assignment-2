dee69c03db3215e62abe2339b82983a8d267bfb4
Naimul Hoque <nh15775@my.bristol.ac.uk>
2017-03-07 22:22:49 +0000

    Started Peak Detector
    Ctrl In and Ctrl Out lines are working.
    Peak detector process will correctly replace the center index with the
    peak.

--- dataProcessor/dataConsume.vhd
+++ dataProcessor/dataConsume.vhd
@@ -1,22 +1,22 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.common_pack.all;
 
 entity dataConsume is
-    port (
-	  clk          :	in std_logic;
-		reset        :	in std_logic; -- synchronous reset
-		start        : in std_logic; -- goes high to signal data transfer
-		numWords_bcd : in BCD_ARRAY_TYPE(2 downto 0);
-		ctrlIn       : in std_logic;
-		ctrlOut      : out std_logic;
-		data         : in std_logic_vector(7 downto 0);
-		dataReady    : out std_logic;
-		byte         : out std_logic_vector(7 downto 0);
-		seqDone      : out std_logic;
-		maxIndex     : out BCD_ARRAY_TYPE(2 downto 0);
-		dataResults  : out CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1) -- index 3 holds the peak
+    port(
+	clk          : in std_logic;
+	reset        : in std_logic; -- synchronous reset
+	start        : in std_logic; -- goes high to signal data transfer
+	numWords_bcd : in BCD_ARRAY_TYPE(2 downto 0);
+	ctrlIn       : in std_logic;
+	ctrlOut      : out std_logic;
+	data         : in std_logic_vector(7 downto 0);
+	dataReady    : out std_logic;
+	byte         : out std_logic_vector(7 downto 0);
+	seqDone      : out std_logic;
+	maxIndex     : out BCD_ARRAY_TYPE(2 downto 0);
+	dataResults  : out CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1) -- index 3 holds the peak
   	);
 end dataConsume;		
 
@@ -25,46 +25,49 @@ architecture Behavioral of dataConsume is
 ----------------------------------------------------------- ASSIGN SIGNALS
 type state_type is (S0, S1, S2, S3, S4, S5);
 signal curState, nextState: state_type;
-signal ctrlIn_reg, ctrlIn_edge, ctrlOut_reg : STD_LOGIC;
-signal byte_reg, data_reg : UNSIGNED(7 downto 0);
+signal ctrlIn_reg, ctrlIn_edge, ctrlOut_reg, seqDone_reg : STD_LOGIC := '0';
+signal byte_reg, data_reg : UNSIGNED(7 downto 0) := X"00";
 signal numWords_reg : BCD_ARRAY_TYPE(2 downto 0);
-signal index, index_max, numWords_int : INTEGER;
+signal index, index_max, numWords_int : INTEGER := 0;
 -----------------------------------------------------------
 begin
 
-  combi_nextState: process(curState, numWords_int, start)
-  variable numWords_int3 : INTEGER := numWords_int; 
+  combi_nextState: process(curState, ctrlIn_edge, numWords_int, start)
+  variable numWords_toMax : INTEGER; 
     begin
+      numWords_toMax := 0;
       ctrlOut_reg <= '0';
       case curState is
         
         when S0 =>
-	        if numWords_int3 /= 0 AND start = '1' then 
-	          ctrlOut_reg <= NOT ctrlOut_reg;
-		        nextState <= S1;
-	        elsif numWords_int3 /= 0 AND start = '0' then
-		        nextState <= S0;
-	        end if;
+          seqDone <= '0';
+	  if numWords_toMax /= numWords_int AND start = '1' then 
+	    ctrlOut_reg <= NOT ctrlOut_reg;
+	    nextState <= S1;
+	  elsif numWords_toMax /= numWords_int AND start = '0' then
+            nextState <= S0;
+	  end if;
 	
-	      when S1 => 
-	        ctrlOut_reg <= NOT ctrlOut_reg;
-	        nextState <= S2;
+	when S1 => 
+	  ctrlOut_reg <= NOT ctrlOut_reg;
+	  nextState <= S2;
 
-	      when S2 => 
-	        if ctrlIn_edge = '1' AND numWords_int3 /= 0 then
-		        numWords_int3 := numWords_int3 - 1;
-		        ctrlOut_reg <= NOT ctrlOut_reg;
-		        nextState <= S2;
-	        elsif(numWords_int3 = 0) then
-		        nextState <= S3;
-	        else
-	          ctrlOut_reg <= NOT ctrlOut_reg; 
-	          nextState <= S0;
-	        end if;
+	when S2 => 
+	if ctrlIn_edge = '1' AND numWords_toMax /= numWords_Int then
+          numWords_toMax := numWords_toMax + 1;
+          ctrlOut_reg <= NOT ctrlOut_reg;
+	  nextState <= S2;
+	elsif(numWords_toMax = numWords_Int) then
+          nextState <= S3;
+	else
+	  ctrlOut_reg <= NOT ctrlOut_reg; 
+	  nextState <= S0;
+	end if;
 	
-	      when S3 =>
-	        ctrlOut_reg <= NOT ctrlOut_reg;
-		      nextState <= S0;
+	when S3 =>
+          seqDone_reg <= '1';
+	  ctrlOut_reg <= NOT ctrlOut_reg;
+	  nextState <= S0;
 	  
         when others =>
           nextState <= S0;
@@ -97,24 +100,38 @@ begin
     numWords_full_int := numWord_int2 * 100 + numWord_int1 * 10 + numWord_int0;
     numWords_int <= numWords_full_int;
   end process;
-
+-----------------------------------------------------------------------------
+--  combi_convertIndexINTtoBCD : process(index_max)
+--  variable index_BCD_full : UNSIGNED(11 downto 0);
+--  variable index_BCD_STD : STD_LOGIC_VECTOR(11 downto 0);
+--  variable index_BCD_1, index_BCD_2, index_BCD_3 : STD_LOGIC_VECTOR(3 downto 0);
+--  begin
+--    index_BCD_full := TO_UNSIGNED(index, 12);
+--    index_BCD_STD := STD_LOGIC_VECTOR(index_BCD_full);
+--    index_BCD_1 := index_BCD_STD(11 downto 8);
+--    index_BCD_2 := index_BCD_STD(7 downto 4);
+--    index_BCD_3 := index_BCD_STD(3 downto 0);
+--    maxIndex <= (index_BCD_1, index_BCD_2, index_BCD_3);
+--  end process;
 -----------------------------------------------------------------------------
   -- Main process, will try to find the peak byte.
-  comb_checkMaxByte : process(data_reg)
-  variable toUnsigned : UNSIGNED(11 downto 0);
+  combi_detectPeak : process(data_reg)
+  variable data_toUnsigned : UNSIGNED(7 downto 0) := X"00";
   variable data_max : UNSIGNED(7 downto 0) := X"00";
+  variable shiftValue : INTEGER := -1;
   begin
-    --data_max <= X"00";
-    if (data_reg > data_max) then
+    data_toUnsigned := UNSIGNED(data_reg);
+    if (TO_INTEGER(data_toUnsigned) > TO_INTEGER(data_max)) then
+      index <= index + 1;
+      index_max <= index;
       data_max := data_reg;
       dataResults(3) <= STD_LOGIC_VECTOR(data_max);
-      --index_max <= index;
-      --toUnsigned := TO_UNSIGNED(index_max, 12);
-      --maxIndex <= (STD_LOGIC_VECTOR(toUnsigned(11 downto 8)), STD_LOGIC_VECTOR(toUnsigned(7 downto 4)), STD_LOGIC_VECTOR(toUnsigned(3 downto 0)));
+      shiftValue := 0;
     else
+      index <= index + 1;
+      shiftValue := shiftValue + 1;
       data_max := data_max;
-      dataResults(4) <= STD_LOGIC_VECTOR(data_reg);
-      --index_max <= index_max;
+      dataResults((3 + shiftValue) mod 7) <= STD_LOGIC_VECTOR(data_reg);
     end if;
   end process;
 
@@ -150,17 +167,17 @@ begin
     if RISING_EDGE(clk) then
       if reset = '1' then
         curState <= S0;
-        index <= 0;
+        --index <= 0;
         --ctrlOut_reg <= '1';
         --dataReady <= '0';
         --byte <= X"00";
-        maxIndex <= (X"0", X"0", X"0");
+        --maxIndex <= (X"0", X"0", X"0");
         --dataResults <= (X"00", X"00", X"00", X"00", X"00", X"00", X"00"); 
-        seqDone <= '0';
+        --seqDone <= '0';
       else
         numWords_reg <= numWords_bcd;
- 	      curState <= nextState;
- 	      --if ctrlIn_edge = '1' then
+ 	curState <= nextState;
+ 	--if ctrlIn_edge = '1' then
           --ctrlOut_reg <= NOT ctrlOut_reg;
           --data_reg <= UNSIGNED(data);
           --index <= index + 1;
@@ -173,6 +190,7 @@ begin
     end if;
   end process;
   
+  seqDone <= seqDone_reg;
   ctrlOut <= ctrlOut_reg;
   byte <= STD_LOGIC_VECTOR(byte_reg);
 
